814a7dcd0ebc4958bf3ce5fdc804e040cc7a4cc5
AstorBald <abdif5909@gmail.com>
2024-11-22 21:41:27 +0700

    Update index.js

--- HappyMensive/js/index.js
+++ HappyMensive/js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Happy Mensive Emma❣️'.split('')
+const text = 'Happy Mensive Sayang❣️'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
